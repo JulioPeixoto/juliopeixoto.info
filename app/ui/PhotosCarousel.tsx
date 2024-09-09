921327f10b7463cea4c3bb61bc0c92bcec393aff
Julio Rafael <juliorafaelnft@hotmail.com>
2024-09-09 12:08:44 -0300

    Atualizando Carousel ui

--- app/ui/PhotosCarousel.tsx
+++ app/ui/PhotosCarousel.tsx
@@ -10,6 +10,9 @@ export function PhotosCarousel() {
         placeholder={undefined}
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}
+        autoplay={true}
+        autoplayDelay={3000}
+        loop={true}
         navigation={({ setActiveIndex, activeIndex, length }) => (
           <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
             {new Array(length).fill("").map((_, i) => (
