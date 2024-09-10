5a76fd3b39960a417c9f87f608c508275bdee1a2
Júlio Rafael Souza Peixeto <4063090@banese.com.br>
2024-09-10 13:57:10 -0300

    Atualizando Projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,21 @@ import Projects from "../ui/Projects";
 export default function Home() {
   return (
     <main className="">
-        <Projects />
+      <section className="mb-4 pb-4">
+        <h2 className="text-3xl font-semibold mb-3 pb-3">
+          Projects
+          <span className="block text-slate-500 font-normal text-2xl">
+            Lorem ipsum dolor. laudantium eius aliquid
+          </span>
+        </h2>
+        <p className="text-lg md:text-xl leading-normal prose prose-lg">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci vel
+          rem ab minus quidem perferendis officiis recusandae distinctio
+          quisquam facilis ducimus ipsam quia, repellat iusto, libero nesciunt
+          iure illo dolores.
+        </p>
+      </section>
+      <Projects />
     </main>
   );
 }
--- app/ui/Projects.tsx
+++ app/ui/Projects.tsx
@@ -32,13 +32,6 @@ export default function Projects() {
 
   return (
     <section className="mb-3 pb-3">
-      <h2 className="text-3xl font-semibold mb-3 pb-3">
-        Projects
-        <span className="block text-slate-500 font-normal text-2xl">
-          Lorem ipsum dolor. laudantium eius aliquid
-        </span>
-      </h2>
-
       <div className="grid grid-cols-1 gap-5">
         {Proj.map((project, index) => (
           <div
