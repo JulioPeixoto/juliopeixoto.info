44484170841d6d21c94b8ef6e1518f44c9704b4c
Julio Rafael <juliorafaelnft@hotmail.com>
2024-09-15 23:12:19 -0300

    Mudança no gap do header

--- app/experience/ui/Info.tsx
+++ app/experience/ui/Info.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Info() {
@@ -6,26 +7,32 @@ export default function Info() {
       <h1 className="font-semibold tracking-tight text-4xl mb-3 pb-3 ">Work</h1>
       <section className="md:text-lg text-base leading-normal mb-2 pb-2 *:mb-4">
         <p>
-          I dont have a long career, but I think its a good start. In
-          my two-year career Ive worked at a startup, which was essential for
-          me to learn a lot of things, and now I work for a large banking
-          company.
+          And here is my professional experience until now, I went through
+          Edtech startup where I was able to learn a lot, and after a year I
+          passed a selection process in the{" "}
+          <Link
+            target="_blank"
+            href="https://www.banese.com.br/"
+            className="text-blue-700 hover:text-blue-200"
+          >
+            State Bank of Sergipe
+          </Link>{" "}
+          where I am today. Both as a Software Engineer.
         </p>
         <p>
           So far Ive met a lot of focused and dedicated people, with a lot of
-          potential, from whom Ive learned and am learning every day, bosses
-          and coworkers that Ill keep with me and with my evolution.
+          potential, from whom Ive learned and am learning every day, bosses and
+          coworkers that Ill keep with me and with my evolution.
         </p>
         <p>
-          I have a lot to learn and a lot of new people to meet, paths to
-          follow, and a long market to get, I intend to leave Brazil at the
-          end of my degree, and go to an international market, I dont know yet
-          I want to go, but Im sure Ill do my best.
+          In the places where I worked, the stack was very similar, working with
+          Python and JavaScript. With frameworks like as Django and FastAPI; LangChain;
+          React and Nextjs.
         </p>
         <p>
-          Here are the companies Ive worked until now, all of them very good
-          and essential to my development, where Ive made friends in all of
-          them and have enormous affection.
+          Here are the companies i have worked until now, all of them very good and
+          essential to my development, where Ive made friends in all of them and
+          have enormous affection.
         </p>
       </section>
     </div>
