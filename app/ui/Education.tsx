6ea053c32c2db0473a2335ae5e4522835dd3b24f
Julio Rafael <juliorafaelnft@hotmail.com>
2024-11-19 18:31:53 -0300

    change: Education

--- app/ui/Education.tsx
+++ app/ui/Education.tsx
@@ -7,9 +7,9 @@ export default function Education() {
       <h1 className="font-semibold text-3xl">Education</h1>
       <span className="block text-slate-500 font-normal text-xl">B.Sc</span>
       <p className="mb-2 pb-2">
-        Im currently studying computer science at the
+        Currently im graduating in computer science at the
         <Link target="_blank" className="text-blue-700 hover:text-blue-200 md:text-lg" href="https://www.unit.br/en/undergraduate"
-        > Tiradentes University</Link> in Sergipe. I intend to do an M.Sc. and go on to a Ph.d in a AI area.
+        > Tiradentes University</Link> in Sergipe and intend to do an M.Sc. 
       </p>
     </section>
   );
